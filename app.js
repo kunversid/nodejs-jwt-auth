bf31cbc730a21c00c5ce72b1239368502277746a
Kunver Siddharth <kunversiddharth@outlook.com>
2021-06-20 23:12:58 +0530

    Cookie Basics

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
@@ -10,6 +11,7 @@ dotenv.config({ path: ".env" });
 // middleware
 app.use(express.static("public"));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set("view engine", "ejs");
@@ -29,3 +31,19 @@ mongoose
 app.get("/", (req, res) => res.render("home"));
 app.get("/smoothies", (req, res) => res.render("smoothies"));
 app.use(authRoutes);
+
+//cookies
+app.get("/set-cookies", (req, res) => {
+  // res.setHeader('Set-Cookie', "newUser=true");
+  res.cookie('newUser', false);
+  res.cookie('isEmployee', true, { maxAge: 1000 * 60, httpOnly: true }); //Cookie will be stored for a minute 
+  //secure: true only accessible through https request;
+  // httpOnly: cannot access using document.cookie from frontend
+  res.send("You got the cookies!");
+});
+
+app.get("/read-cookies", (req, res) => {
+  const cookies = req.cookies;
+  console.log(cookies);
+  res.json(cookies);
+});

126ad5228f06c743870f768a77ad24b6c9d5c6df
Kunver Siddharth <kunversiddharth@outlook.com>
2021-06-25 21:01:16 +0530

    New User Signup - I

--- app.js
+++ app.js
@@ -32,18 +32,3 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/smoothies", (req, res) => res.render("smoothies"));
 app.use(authRoutes);
 
-//cookies
-app.get("/set-cookies", (req, res) => {
-  // res.setHeader('Set-Cookie', "newUser=true");
-  res.cookie('newUser', false);
-  res.cookie('isEmployee', true, { maxAge: 1000 * 60, httpOnly: true }); //Cookie will be stored for a minute 
-  //secure: true only accessible through https request;
-  // httpOnly: cannot access using document.cookie from frontend
-  res.send("You got the cookies!");
-});
-
-app.get("/read-cookies", (req, res) => {
-  const cookies = req.cookies;
-  console.log(cookies);
-  res.json(cookies);
-});
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const jwt = require('jsonwebtoken');
 
 //handle errors
 const handleErrors = (err) => {
@@ -20,6 +21,14 @@ const handleErrors = (err) => {
   return errors;
 };
 
+const maxAge = 3 * 24 * 60 * 60;
+
+const createToken = (id) => {
+  return jwt.sign({ id }, 'kunver siddharth secret', {
+    expiresIn: maxAge
+  }); //SECRET
+}
+
 module.exports.signup_get = (req, res) => {
   res.render("signup");
 };
@@ -32,7 +41,9 @@ module.exports.signup_post = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.create({ email, password });
-    res.status(201).json(user);
+    const token = createToken(user._id);
+    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(201).json({ user: user._id });
   } catch (err) {
     const errors = handleErrors(err);
     res.status(400).json({ errors });

dc9c0ea2958c85bc1e06cabbbf925c8145d225b8
Kunver Siddharth <kunversiddharth@outlook.com>
2021-06-28 14:47:30 +0530

    User Login Setup

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,6 +6,16 @@ const handleErrors = (err) => {
   console.log(err.message, err.code);
   let errors = { email: "", password: "" };
 
+  //incorrect email
+  if (err.message === 'incorrect email') {
+    errors.email = 'The email is not registered';
+  }
+
+  //incorrect password
+  if (err.message === 'incorrect password') {
+    errors.password = 'The password is not correct';
+  }
+
   //duplicate error code
   if (err.code === 11000) {
     errors.email = "The email is already registered";
@@ -52,5 +62,14 @@ module.exports.signup_post = async (req, res) => {
 
 module.exports.login_post = async (req, res) => {
   const { email, password } = req.body;
-  res.send("user login");
+
+  try {
+    const user = await User.login(email, password);
+    const token = createToken(user._id);
+    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(200).json({ user: user._id })
+  } catch (err) {
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
+  }
 };
